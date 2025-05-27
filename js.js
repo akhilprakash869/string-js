a4ed44572ec8d5cb80c8199ac6b9b5633345b6f4
Akhi-Prakash <nathantheresa22@gmail.com>
2025-05-27 18:11:04 +0530

    primitive-data-type-string

---
+++ js.js
@@ -0,0 +1,6 @@
+//Primitive data types string 
+//string declare three types single quote, double quote, and template literals;
+let str='hello';
+let str1="hello world";
+let str3=`Hello ${str1}`;
+console.log(str3);
